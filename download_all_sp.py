1a4d48553d2f555b23f005642aa7446fef73d30e
h-pgy <hpougy@gmail.com>
2020-05-06 15:04:47 -0300

    script para baixar tudo de sp

---
+++ download_all_sp.py
@@ -0,0 +1,15 @@
+#script para baixar todos arquivos de SP
+
+
+def download_all_sp(sim_downloader, file_dir='SIM_SP'):
+    sim_downloader.go_to_sim()
+    files_sp = sim_downloader.filter_files(estado='sp')
+    sim_downloader.download_files(files_sp, file_dir)
+
+if __name__ == "__main__":
+
+    from SimDownloader import SIMDownloader
+
+    sim_downloader = SIMDownloader()
+
+    download_all_sp(sim_downloader)
